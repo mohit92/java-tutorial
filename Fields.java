b0d09ad6f7616a5c734fad7eb7f6b398225271ef
Mohit Agarwal <mohit.agarwal@helpshift.com>
2014-05-22 16:12:55 +0530

    demonstrate different type of variables

---
+++ Fields.java
@@ -0,0 +1,14 @@
+class Fields
+{
+    int num1;   //instance variables
+    float num2;
+    char ch;
+
+    static int  static_number; // static/class variable
+
+    public static void main(String [] args )
+    {
+        int i; //local variable
+    }
+
+}
